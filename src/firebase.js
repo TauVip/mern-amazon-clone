bc2a0b7a8d0c5f792d112d16f1d5b3951235b04d
TauVip <Abylai1393@gmail.com>
2021-04-21 18:55:09 +0600

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyBOVj3Welj93o-2v9rPw4p-uf2k0eZTknI',
+  authDomain: 'tiktok-clone-a8946.firebaseapp.com',
+  projectId: 'tiktok-clone-a8946',
+  storageBucket: 'tiktok-clone-a8946.appspot.com',
+  messagingSenderId: '955372901125',
+  appId: '1:955372901125:web:1cb84c507d9628b4ba1d6c'
+}
